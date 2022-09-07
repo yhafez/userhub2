a191af2973775aac796e4e13570fe808556d5b97
Yahya Hafez <yhafez3@gmail.com>
2022-09-06 21:10:31 -0500

    fix: eslint errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,65 @@
 // /src/components/Header.js
 
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
-import{ storeCurrentUser, clearCurrentUser } from '../auth';
+import { storeCurrentUser, clearCurrentUser } from "../auth";
 
-import './Header.css';
+import "./Header.css";
 
-const Header = ({ 
-    currentUser,
-    setCurrentUser,
-    userList
-}) => {
-
+const Header = ({ currentUser, setCurrentUser, userList }) => {
     const [selectedUser, setSelectedUser] = useState();
 
     useEffect(() => setSelectedUser(userList[0]), [userList]);
-    
+
     const handleSubmit = (event) => event.preventDefault();
 
     const handleSelectChange = (event) => {
         const id = event.target.value;
-        const user = userList.find(user => user.id == id);
+        const user = userList.find((user) => user.id === id);
         setSelectedUser(user);
-    }
+    };
 
     const handleUserLogin = (event) => {
         storeCurrentUser(selectedUser);
         setCurrentUser(selectedUser);
-    }
+    };
 
     const handleUserLogout = (event) => {
         setSelectedUser(userList[0]);
         clearCurrentUser();
         setCurrentUser(null);
-    }
+    };
 
-    return(
-
+    return (
         <header>
             <h1>Welcome to UserHub</h1>
-            <form
-            className="user-select"
-            onSubmit={ handleSubmit } >
-                {
-                    currentUser
-                    ? 
-                        <>
-                            <NavLink to="/posts" activeClassName="current">POSTS</NavLink>
-                            <NavLink to="/todos" activeClassName="current">TODOS</NavLink>
-                            <button onClick={ handleUserLogout }>LOG OUT</button>
-                        </>
-                    : <>
-                        <select onChange={ handleSelectChange } >{
-                            userList.map(user => (
-                                <option key={ user.id } value={ user.id }>
-                                    { user.username }
-                                </option>
-                            ))
-                        }</select>
-                        <button onClick={ handleUserLogin }>LOG IN</button>
+            <form className="user-select" onSubmit={handleSubmit}>
+                {currentUser ? (
+                    <>
+                        <NavLink to="/posts" activeClassName="current">
+                            POSTS
+                        </NavLink>
+                        <NavLink to="/todos" activeClassName="current">
+                            TODOS
+                        </NavLink>
+                        <button onClick={handleUserLogout}>LOG OUT</button>
                     </>
-                }
+                ) : (
+                    <>
+                        <select onChange={handleSelectChange}>
+                            {userList.map((user) => (
+                                <option key={user.id} value={user.id}>
+                                    {user.username}
+                                </option>
+                            ))}
+                        </select>
+                        <button onClick={handleUserLogin}>LOG IN</button>
+                    </>
+                )}
             </form>
         </header>
     );
-}
+};
 
 export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,16 @@
 // /src/components/index.js
 
-import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => (<div id='App'><h1>Hello, World</h1></div>);
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+const App = () => (
+    <div id="App">
+        <h1>Hello, World</h1>
+    </div>
 );
 
-export { default as Header } from './Header';
-export { default as UserPosts } from './UserPosts';
-export { default as UserTodos } from './UserTodos'
+ReactDOM.render(<App />, document.getElementById("root"));
+
+export { default as Header } from "./Header";
+export { default as UserPosts } from "./UserPosts";
+export { default as UserTodos } from "./UserTodos";
